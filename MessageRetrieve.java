d1c7daf6236fffa6b5e7298f72d4cc766af46554
ANKIT KUMAR <ankit247shbm@gmail.com>
2018-01-12 20:51:26 +0530

    Create MessageRetrieve.java

---
+++ MessageRetrieve.java
@@ -0,0 +1,11 @@
+/* Basic code to Decrypt a CipherText. */
+
+public class Go_backNProtocol
+{
+  public static void main()
+   {
+     String cipherText = new String();
+     cipherText = "d0m2u1h2g1p2t0q1k0z1s2j1";
+     
+   }
+}
