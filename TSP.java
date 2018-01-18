b72729a1b1a367ff61734926e8cc95a69c253cda
ANKIT KUMAR <ankit247shbm@gmail.com>
2018-01-18 22:19:05 +0530

    Update TSP.java

--- TSP.java
+++ TSP.java
@@ -1,1 +1,30 @@
 /***** Routing Table Formation Algorithm. *****/
+
+
+import java.io.*;
+
+class TravelSalesManProblem 
+{
+    static void TravelSalesManProblem(int[][] A, int n)
+      {
+          int sum = 0;
+          int input = 0;                            // Beginning of the Node.
+          System.out.println(A[1][5] + " ");
+          ; // sizeof A[][].
+//--------------------------------------------------------------------
+          
+          
+      }
+    public static void main (String[] args) 
+	  {
+          int[][] A = {{0, 0, 0, 60, 0, 16, 0, 0}, {0, 0, 8, 0, 0, 4, 0, 0}, {0, 8, 0, 0, 0, 0, 0, 0}, {60, 0, 0, 0, 11, 17, 0, 0}, {0, 0, 0, 11, 0, 0, 0, 6}, {16, 4, 0, 17, 0, 0, 16, 0}, {0, 0, 0, 0, 0, 16, 0, 12}, {0, 0, 0, 0, 6, 0, 12, 0}};
+          int n = 8;                                 // Nos. of Connecting Router.             
+//          System.out.println(A.length());
+//          System.out.print(A[0][3] + " ");
+//          System.out.print(A[0][5] + " ");
+//          System.out.println(A[1][5] + " ");
+//--------------------------------------------------------------------
+          TravelSalesManProblem(A, n);                     // Allot Time to Router.
+          
+	  }
+}
