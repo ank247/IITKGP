6bb32354f1c5bb88daf22adfbd520373ec29f205
ANKIT KUMAR <ankit247shbm@gmail.com>
2018-01-23 20:56:22 +0530

    Update SteppedMotor.java

--- SteppedMotor.java
+++ SteppedMotor.java
@@ -1,6 +1,11 @@
 /***** Principals of Stepped-Motors.*****/
 /* 
   Q1.Mechanism of Electronic Controller?
-  Q2.How does Stepped Motors work?
+  Q2.Various Basic Movements?
+  --> i) Straight line.   ii)Axial Rotation 
+  Q3.How does Stepped Motors work?
+  
+  
+  
   
 */
