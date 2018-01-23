9df8af0aadfea3b52603e93f44fcfb74549aa023
ANKIT KUMAR <ankit247shbm@gmail.com>
2018-01-24 00:23:49 +0530

    Update SteppedMotor.java

--- SteppedMotor.java
+++ SteppedMotor.java
@@ -1,23 +1,20 @@
 /***** Principals of Stepped-Motors.*****/
 /* 
-  Q0.Quantity?
-  --> i)Weight.  ii)Time.  iii)Volume.  iv)
   Q1.Units?
-  --> i)Metre.  ii)Radian.  iii)Kilogram. iv)Time. v)Mole. vi)Kelvin. vii)
-  Q1.Mechanism of Electronic Controller?
-  Q2.Different Spatial Occupiants?
-  --> i)1D-Point.  ii)2D-Area/Planar. iii)Volume.
+  --> i)Metre.  ii)Radian.  iii)Kilogram. iv)Second. v)Mole. vi)Kelvin. vii)Coulomb.
+  Q2.Mechanism of Electronic Controller?
+  Q3.Different Spatial Occupiants?
+  --> i)1D-Point.  ii)2D-Area/Planar. iii)3D-Volume.
   Q4. Different Shapes & Structures?
   --> i)Point.  ii) St. Line.  iii)Circle.
-  Q2.Co-ordinate Systems?
-  --> i)Radial Vector(r, theta).    ii)
-  Q3.Various Basic Movements?
+  Q5.Co-ordinate Systems?
+  --> i)Radial Vector(r, theta).    ii)Scalar.
+  Q6.Various Basic Movements?
   --> i) Straight line.   ii)Axial Rotation. iii)
-  Q4.Various Joints & Linkages?
+  Q7.Various Joints & Linkages?
   --> i)Sliding.  ii)
-  Q5.How does Stepped Motors work?
-  
-  
-  
-  
+  Q8.How does Stepped Motors work?
+  Q9.
+  --> i)Cube. ii)Cuboid. iii)Rhomboid. iv)Hexagonal.  v)Triclinic. vi)Monoclinic.  vi)Tetragonal.
 */
+
