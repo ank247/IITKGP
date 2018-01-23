eb97fff4df8fcd9f47cfb98d0d79ee0af44a6600
ANKIT KUMAR <ankit247shbm@gmail.com>
2018-01-23 23:23:45 +0530

    Update SteppedMotor.java

--- SteppedMotor.java
+++ SteppedMotor.java
@@ -1,12 +1,20 @@
 /***** Principals of Stepped-Motors.*****/
 /* 
+  Q0.Quantity?
+  --> i)Weight.  ii)Time.  iii)Volume.  iv)
+  Q1.Units?
+  --> i)Metre.  ii)Radian.  iii)Kilogram. iv)Time. v)Mole. vi)Kelvin. vii)
   Q1.Mechanism of Electronic Controller?
+  Q2.Different Spatial Occupiants?
+  --> i)1D-Point.  ii)2D-Area/Planar. iii)Volume.
+  Q4. Different Shapes & Structures?
+  --> i)Point.  ii) St. Line.  iii)Circle.
   Q2.Co-ordinate Systems?
   --> i)Radial Vector(r, theta).    ii)
   Q3.Various Basic Movements?
   --> i) Straight line.   ii)Axial Rotation. iii)
   Q4.Various Joints & Linkages?
-  -->
+  --> i)Sliding.  ii)
   Q5.How does Stepped Motors work?
   
   
