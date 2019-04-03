ed429fc63f170d3a40658009360c1fad172cce01
ANKIT KUMAR <ankit247shbm@gmail.com>
2019-04-03 18:34:00 +0530

    Create A_1a.c

---
+++ CourseWork/os/interpreter/A_1a.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+  char x[20];
+  int a[200];
+  do{
+      scanf("%s", x);
+      if((x[1]=='=')&&(x[3]=='+'))
+       {
+         a[x[0]]=a[x[2]]+a[x[4]];
+       }
+       if((x[1]=='=')&&(x[2]<60))
+       {
+         a[x[0]]=x[2]-48;
+       }
+       elif((x[1]=='=')&&(x[2]>60)&&(x[3]!='+'))
+        {
+          a[x[0]]=a[x[2]];
+        }
+       if(x[1]=='r')
+       {
+         printf("%d\n",a[x[6]]);
+         sleep(1);
+       }
+    }while(1);
+}
+
+/*
+Input:
+
+k=7
+u=k
+print(u)
+
+Output: 7
+*/
