2f0b9bd2538e58ff87342c13dac2d7e6865672b7
ANKIT KUMAR <ankit247shbm@gmail.com>
2019-06-09 09:46:52 +0000

    Update client.c

--- CourseWork/networks/MA61010/tcp/asg01/client.c
+++ CourseWork/networks/MA61010/tcp/asg01/client.c
@@ -16,7 +16,6 @@ main()
  sockfd=socket(AF_INET,SOCK_STREAM,0);
  sa.sin_family=AF_INET;
  sa.sin_addr.s_addr=inet_addr("127.0.0.1");
-// sa.sin_port=6001;
  printf("Give server port no.:");
  scanf("%d",&a);
  sa.sin_port=a;
@@ -27,10 +26,6 @@ main()
  send(sockfd,&a,4,0);
  recv(sockfd,&a,4,0);
  printf("\nDouble value:%d",a);
-// send(sockfd,&a,4,0);
-// sleep(12);
-// recv(sockfd,q,50,0);
-// printf("\nserver sent %s\n",q);
  return 0;
 }
 
