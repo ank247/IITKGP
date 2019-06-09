1181d406401fcb596f0905caf8a9d220b3c49383
ANKIT KUMAR <ankit247shbm@gmail.com>
2019-06-09 17:33:05 +0000

    Add files via upload

--- CourseWork/networks/MA61010/tcp/asg07/client.c
+++ CourseWork/networks/MA61010/tcp/asg07/client.c
@@ -1,1 +1,36 @@
 
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<fcntl.h>
+
+main()
+{
+ int sockfd,len,i,a,p[10];
+ struct sockaddr_in sa;
+ len=sizeof(sa);
+ sockfd=socket(AF_INET,SOCK_STREAM,0);
+ sa.sin_family=AF_INET;
+ sa.sin_addr.s_addr=inet_addr("127.0.0.1");
+ printf("Give server port no.:\n");
+ scanf("%d",&a);
+ sa.sin_port=a;
+ i=connect(sockfd,(struct sockaddr *)&sa,len);
+ printf("Enter 10 nos.");
+ for(i=0;i<10;i++)
+ {
+  scanf("%d",&p[i]);
+  a=p[i];
+  send(sockfd,&a,4,0);
+  while(recv(sockfd,&a,4,0))
+  {
+   recv(sockfd,&a,4,0);
+   printf("%d\n",a);
+  }
+ }
+ return 0;
+}
+
+
