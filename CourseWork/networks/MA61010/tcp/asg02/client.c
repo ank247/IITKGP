0e774d4f4595924fafbcea67b1ca81f9a01c44a8
ANKIT KUMAR <ankit247shbm@gmail.com>
2019-06-09 10:31:13 +0000

    Add files via upload

--- CourseWork/networks/MA61010/tcp/asg02/client.c
+++ CourseWork/networks/MA61010/tcp/asg02/client.c
@@ -1,1 +1,37 @@
 
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<fcntl.h>
+
+main()
+{
+ int sockfd,len,i,a;
+ char q[200];
+ struct sockaddr_in sa;
+ len=sizeof(sa);
+ sockfd=socket(AF_INET,SOCK_STREAM,0);
+ sa.sin_family=AF_INET;
+ sa.sin_addr.s_addr=inet_addr("127.0.0.1");
+ printf("Give server port no.:");
+ scanf("%d",&a);
+ sa.sin_port=a;
+ i=connect(sockfd,(struct sockaddr *)&sa,len);
+ printf("(%d %d)\n",sockfd,i);
+ printf("Give a number for server");
+ scanf("%d",&a);
+ send(sockfd,&a,4,0);
+ recv(sockfd,&a,4,0);
+ printf("\nDouble value:%d",a);
+// send(sockfd,&a,4,0);
+// sleep(12);
+// recv(sockfd,q,50,0);
+// printf("\nserver sent %s\n",q);
+ return 0;
+}
+
+
+
---
+++ CourseWork/networks/MA61010/tcp/asg02/server.c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<fcntl.h>
+
+main()
+{
+ int sockfd,fd,len,i,p;
+ char b[200];
+ struct sockaddr_in sa,ca;
+ len=sizeof(sa);
+ sockfd=socket(AF_INET,SOCK_STREAM,0);
+ sa.sin_family=AF_INET;
+ sa.sin_addr.s_addr=INADDR_ANY;
+ printf("Give self port no.:");
+ scanf("%d",&sa.sin_port);
+ i=bind(sockfd,(struct sockaddr *)&sa,len);
+ printf("(%d %d)\n",sockfd,i);
+ listen(sockfd,5);
+ fd=accept(sockfd,(struct sockaddr *)&ca,&len);
+ printf("[%d]\n",fd);
+ printf("Enter no.:\n");
+ scanf("%d",&p);
+ send(fd,&p,4,0);
+ recv(fd,&p,4,0);
+ printf("client sent %d\n",p);
+ return 0;
+}
+
+
