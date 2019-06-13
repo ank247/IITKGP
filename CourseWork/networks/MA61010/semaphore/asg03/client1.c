3a6c178c2f0ba0c0091f03832ecc15d72be0b821
ANKIT KUMAR <ankit247shbm@gmail.com>
2019-06-13 16:31:38 +0000

    Add files via upload

--- CourseWork/networks/MA61010/semaphore/asg03/client1.c
+++ CourseWork/networks/MA61010/semaphore/asg03/client1.c
@@ -1,1 +1,23 @@
 
+
+#include<stdio.h>
+#include<sys/sem.h>
+
+main()
+{
+ int p,x,i=0,n;
+ printf("Give semaphore no");
+ scanf("%d",&p);
+ printf("Enter no. to be sent:");
+ scanf("%d",&n);
+ semctl(p,0,SETVAL,n);
+ do
+ {
+  printf("give a no. to send:");
+  scanf("%d",&x);
+  semctl(p,0,SETVAL,x);
+  i+=1;
+ }while(i<n);
+}
+
+
