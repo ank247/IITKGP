39bfbfd39563ece133905196a568770ccd559175
TechTed <ankit247shbm@gmail.com>
2019-12-24 16:03:49 +0000

    Update client4.c

--- CourseWork/networks/CS39006/test2/client4.c
+++ CourseWork/networks/CS39006/test2/client4.c
@@ -1,1 +1,42 @@
 
+
+void sys_init(){
+
+}
+
+void initiate_transaction(){
+
+}
+
+void receive_transaction(){
+
+}
+
+void broadcast_transaction(){
+
+}
+
+void validate_transaction(){
+
+}
+
+void vote_transaction(){
+
+}
+
+void receive_vote(){
+
+}
+
+void broadcast_vote(){
+
+}
+
+void reach_consensus(){
+
+}
+
+main()
+{
+
+}
