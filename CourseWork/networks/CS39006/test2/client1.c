4e3f9d8f120021d3505b00fd69c8211a7b379e25
TechTed <ankit247shbm@gmail.com>
2019-12-25 08:14:01 +0000

    Update client1.c

--- CourseWork/networks/CS39006/test2/client1.c
+++ CourseWork/networks/CS39006/test2/client1.c
@@ -12,17 +12,36 @@
 #include<sys/types.h>
 #include<>
 
+
+
 struct Node{
-  
+  int num;
+  int constant=100;
+  struct Node*next = Null;
 };
+
+
 
 struct linkedList{
-
+  struct Node*node;
+  while(node->next != Null)
+  {
+      node=node->next;
+  }
+  void append(num)
+  {
+    node->num=num;
+    node=node->next;
+  }
 };
+
+
 
 void sys_init(){
   int amount=100;
 }
+
+
 
 void initiate_transaction(){
 
